ec7e51d394ca44dd8fd23c369fed04a045f58027
Rav-Ariya <rav-ariya@github.com>
2021-10-29 11:42:06 +1300

    Order page - customer details and order details

---
+++ public/quantitybutton.js
@@ -0,0 +1,32 @@
+
+  
+  function() {
+
+    var $button = $(this);
+    var oldValue = $button.parent().find("input").val();
+
+    if ($button.text() == "+") {
+  	  var newVal = parseFloat(oldValue) + 1;
+  	} else {
+	   // Don't allow decrementing below zero
+      if (oldValue > 0) {
+        var newVal = parseFloat(oldValue) - 1;
+	    } else {
+        newVal = 0;
+      }
+	  }
+
+    $button.parent().find("input").val(newVal);
+
+  }
+
+
+
+
+
+  function increment() {
+    document.getElementById('demoInput').stepUp();
+ }
+ function decrement() {
+    document.getElementById('demoInput').stepDown();
+ }
