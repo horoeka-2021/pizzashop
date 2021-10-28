2104fd5f093e2557fa696421a6dcd41a307c161e
tommy-yoon <taeil.yoon@gmail.com>
2021-10-28 19:55:26 +1300

    add adding a new column, quantity

--- db/seeds/2.orders.js
+++ db/seeds/2.orders.js
@@ -4,11 +4,11 @@ exports.seed = function(knex) {
         .then(function() {
             // Inserts seed entries
             return knex('orders').insert([
-                { id: 1, pizza_id: '1', customer_name: 'Ying', customer_address: 'Auckland', status: "in progress" },
-                { id: 2, pizza_id: '2', customer_name: 'Crystal', customer_address: 'Auckland', status: "in progress" },
-                { id: 3, pizza_id: '3', customer_name: 'Rav', customer_address: 'Auckland', status: "in progress" },
-                { id: 4, pizza_id: '4', customer_name: 'Vimal', customer_address: 'Auckland', status: "in progress" },
-                { id: 5, pizza_id: '5', customer_name: 'Tommy', customer_address: 'Auckland', status: "in progress" },
+                { id: 1, pizza_id: '1', customer_name: 'Ying', customer_address: 'Auckland', status: 'in progress', quantity: 1 },
+                { id: 2, pizza_id: '2', customer_name: 'Crystal', customer_address: 'Auckland', status: 'in progress', quantity: 2 },
+                { id: 3, pizza_id: '3', customer_name: 'Rav', customer_address: 'Auckland', status: 'in progress', quantity: 2 },
+                { id: 4, pizza_id: '4', customer_name: 'Vimal', customer_address: 'Auckland', status: 'in progress', quantity: 3 },
+                { id: 5, pizza_id: '5', customer_name: 'Tommy', customer_address: 'Auckland', status: 'in progress', quantity: 10 },
             ]);
         });
 };
